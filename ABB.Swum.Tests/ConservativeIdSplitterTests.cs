7d4c94af631c3cae5c606f42e85a5d708bb4f419
Patrick Francis <patrick.francis@us.abb.com>
2013-07-09 11:23:33 -0400

    Updated conservative splitter to split acronyms from lowercase letters.

--- ABB.Swum.Tests/ConservativeIdSplitterTests.cs
+++ ABB.Swum.Tests/ConservativeIdSplitterTests.cs
@@ -17,63 +17,20 @@ using ABB.Swum;
 using NUnit.Framework;
 
 
-namespace ABB.Swum.Tests
-{
+namespace ABB.Swum.Tests {
     /// <summary>
     /// Unit tests for ABB.Swum.ConservativeIdSplitter
     /// </summary>
     [TestFixture]
-    public class ConservativeIdSplitterTests
-    {
-        public ConservativeIdSplitterTests()
-        {
+    public class ConservativeIdSplitterTests {
+        private readonly ConservativeIdSplitter splitter;
+
+        public ConservativeIdSplitterTests() {
             splitter = new ConservativeIdSplitter();
         }
 
-        private TestContext testContextInstance;
-        private ConservativeIdSplitter splitter;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
-
         [Test]
-        public void AllSameCase_SingleWord()
-        {
+        public void AllSameCase_SingleWord() {
             //test lowercase word
             string[] actual = splitter.Split("lowercase");
             Assert.AreEqual("lowercase", actual[0]);
@@ -86,42 +43,50 @@ namespace ABB.Swum.Tests
         }
 
         [Test]
-        public void AllSameCase_MultipleWords()
-        {
+        public void AllSameCase_MultipleWords() {
             string[] actual = splitter.Split("lower_case");
-            string[] expected = { "lower", "case" };
+            string[] expected = {"lower", "case"};
             Assert.AreEqual(expected.Length, actual.Length);
-            for (int i = 0; i < actual.Length; i++)
+            for(int i = 0; i < actual.Length; i++)
                 Assert.AreEqual(expected[i], actual[i]);
 
             actual = splitter.Split("HELLO_WORLD");
             expected[0] = "HELLO";
             expected[1] = "WORLD";
             Assert.AreEqual(expected.Length, actual.Length);
-            for (int i = 0; i < actual.Length; i++)
+            for(int i = 0; i < actual.Length; i++)
                 Assert.AreEqual(expected[i], actual[i]);
 
             actual = splitter.Split("howdy_water4melons999");
-            string[] expected2 = { "howdy", "water", "4", "melons", "999" };
+            string[] expected2 = {"howdy", "water", "4", "melons", "999"};
             Assert.AreEqual(expected2.Length, actual.Length);
-            for (int i = 0; i < actual.Length; i++)
+            for(int i = 0; i < actual.Length; i++)
                 Assert.AreEqual(expected2[i], actual[i]);
         }
 
         [Test]
-        public void MixedCase()
-        {
+        public void MixedCase() {
             string[] actual = splitter.Split("HelloWorld");
-            string[] expected = { "Hello", "World" };
+            string[] expected = {"Hello", "World"};
             Assert.AreEqual(expected.Length, actual.Length);
-            for (int i = 0; i < actual.Length; i++)
+            for(int i = 0; i < actual.Length; i++)
                 Assert.AreEqual(expected[i], actual[i]);
 
             actual = splitter.Split("first_secondThird");
-            string[] expected2 = { "first", "second", "Third" };
+            string[] expected2 = {"first", "second", "Third"};
             Assert.AreEqual(expected2.Length, actual.Length);
-            for (int i = 0; i < actual.Length; i++)
+            for(int i = 0; i < actual.Length; i++)
                 Assert.AreEqual(expected2[i], actual[i]);
+        }
+
+        [Test]
+        public void UpperToLowercase() {
+            var actual = splitter.Split("XMLParser");
+            var expected = new[] {"XML", "Parser"};
+            Assert.AreEqual(expected.Length, actual.Length);
+            for(var i = 0; i < actual.Length; i++) {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
         }
     }
 }
--- ABB.Swum/ConservativeIdSplitter.cs
+++ ABB.Swum/ConservativeIdSplitter.cs
@@ -16,40 +16,34 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace ABB.Swum
-{
+namespace ABB.Swum {
     /// <summary>
     /// Splits an identifier on non-alphabetic characters and easy camelcase transitions (lowercase to uppercase).
     /// </summary>
-    public class ConservativeIdSplitter : IdSplitter
-    {
-        private readonly char[] NonAlphabeticCharacters = { '_', '@', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+    public class ConservativeIdSplitter : IdSplitter {
 
         /// <summary>
         /// Splits an identifier on non-alphabetic characters and easy camelcase transitions (lowercase to uppercase).
         /// </summary>
         /// <param name="identifier">The identifier to split</param>
         /// <returns>An array of the words resulting from splitting the identifier.</returns>
-        public override string[] Split(string identifier)
-        {
-            //TODO: this splits on digits individually, and throws them away as meaningless word separators
-            //      Should this be changed to group successive digits together and treat them as potentially-meaningful word?
-            //      For example, in Special123Certificate, 123 might be meaningful to the developer and a useful search term
+        public override string[] Split(string identifier) {
+            //remove any non-word or non-digit characters
+            var id = Regex.Replace(identifier, @"[^\d\p{L}]", " ");
 
-            List<string> splitWords = new List<string>();
-            foreach(string word in Regex.Split(identifier, @"[^\d\p{L}]+"))
-            {
-                if (!string.IsNullOrEmpty(word))
-                {
-                    splitWords.AddRange(SplitOnLowercaseToUppercase(word));
-                }
-            }
-            //foreach (string word in identifier.Split(NonAlphabeticCharacters, StringSplitOptions.RemoveEmptyEntries))
-            //{
-            //    splitWords.AddRange(SplitOnLowercaseToUppercase(word));
-            //}
+            //split numbers from letters
+            id = Regex.Replace(id, @"(\p{L})(\d)", "$1 $2");
+            id = Regex.Replace(id, @"(\d)(\p{L})", "$1 $2");
 
-            return splitWords.ToArray();
+            //split lowercase to uppercase
+            id = Regex.Replace(id, @"(\p{Ll})(\p{Lu})", "$1 $2");
+
+            //split uppercase to lowercase
+            //final uppercase letter is put with lowercase ones
+            id = Regex.Replace(id, @"(\p{Lu})(\p{Lu}\p{Ll})", "$1 $2");
+
+
+            return id.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
         }
 
         /// <summary>
@@ -57,16 +51,13 @@ namespace ABB.Swum
         /// </summary>
         /// <param name="word">The word to be split.</param>
         /// <returns>An array of the subwords resulting from the splits.</returns>
-        private string[] SplitOnLowercaseToUppercase(string word)
-        {
+        private string[] SplitOnLowercaseToUppercase(string word) {
             List<string> splitWords = new List<string>();
             int currentStartIndex = 0; //the index of the beginning of the current subword
-            for (int i = 0; i < word.Length - 1; i++)
-            {
-                if ((char.IsLower(word, i) && char.IsUpper(word, i + 1))
-                    || (char.IsDigit(word, i) && !char.IsDigit(word, i + 1))
-                    || (!char.IsDigit(word, i) && char.IsDigit(word, i + 1)))
-                {
+            for(int i = 0; i < word.Length - 1; i++) {
+                if((char.IsLower(word, i) && char.IsUpper(word, i + 1))
+                   || (char.IsDigit(word, i) && !char.IsDigit(word, i + 1))
+                   || (!char.IsDigit(word, i) && char.IsDigit(word, i + 1))) {
                     //found split point, add left word to list
                     splitWords.Add(word.Substring(currentStartIndex, i - currentStartIndex + 1));
                     currentStartIndex = i + 1;
